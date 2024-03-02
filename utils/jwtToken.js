4833de4efb8208bdcfdec50be5be38e995b97b80
Joy <oyinyeezeala@Joys-MacBook-Air.local>
2024-03-02 17:23:42 +0100

    user token changed

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,18 +1,19 @@
 // create token and saving that in cookies
 const sendToken = (user, statusCode, res) => {
-  const token = user.getJwtToken();
+  const user_token = user.getJwtToken();
 
   // Options for cookies
   const options = {
     expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
     httpOnly: true,
-    Secure: true,
+    sameSite: "none",
+    secure: true,
   };
 
-  res.status(statusCode).cookie("token", token, options).json({
+  res.status(statusCode).cookie("user_token", user_token, options).json({
     success: true,
     user,
-    token,
+    user_token,
   });
 };
 
